c2870763ad19d2fb0847b08e0307369a6ad5101e
chernandez <chernandez@169.254.81.3>
2017-01-30 15:42:31 -0500

    Creación del proyecto Cliente V3

---
+++ ClienteV3/src/java/Connection/Connection.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Connection;
+
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+/**
+ *
+ * @author chernandez
+ */
+public class Connection {
+    
+    private static final Connection connection = new Connection();
+    protected static EntityManagerFactory emf;//Just 1 entity manager factory must be created for all sessions
+
+    public static Connection getInstance() {
+        return connection;
+    }
+
+    public EntityManagerFactory getEntityManagerFactory() {//Singleton pattern
+        if (emf == null) {
+            emf = Persistence.createEntityManagerFactory("PorteriaV3PU");
+        }
+        return emf;
+    }
+
+    public void closeEntityManagerFactory() {
+        if (emf != null) {
+            emf.close();
+            emf = null;
+        }
+    }
+
+    
+}
---
+++ ClienteV3/src/java/Utils/Constants.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Utils;
+
+/**
+ *
+ * @author chernandez
+ */
+public class Constants {
+    
+    // <editor-fold desc="ERRORS" defaultstate="collapsed">
+    public static final int OK = 0;
+    public static final int UNKNOWN_EXCEPTION = 1;
+    public static final int UNKNOWN_EXCEPTION_AT_FINALLY = 2;
+    public static final int NO_RESULT_EXCEPTION = 3;
+    public static final int NO_UNIQUE_RESULT_EXCEPTION = 4;
+    public static final int VALIDATION_ERROR = 5;
+    //</editor-fold>
+    
+    // <editor-fold desc="FACADE ERROR MESSAGES" defaultstate="collapsed">
+    public static final String MESSAGE_VALIDATION_ERROR = "PORTERIA ERROR: VALIDATION ERROR ";
+    public static final String MESSAGE_UNKNOWN_EXCEPTION = "PORTERIA EXCEPTION: UNKNOWN EXCEPTION ";
+    public static final String MESSAGE_UNKNOWN_EXCEPTION_AT_FINALLY = "PORTERIA EXCEPTION: UNKNOWN EXCEPTION AT FINALLY ";
+    public static final String MESSAGE_NO_RESULT_EXCEPTION = "PORTERIA EXCEPTION: NO RESULT EXCEPTION";
+    public static final String MESSAGE_NO_UNIQUE_RESULT_EXCEPTION = "PORTERIA EXCEPTION: NO UNIQUE RESULT EXCEPTION";
+    //</editor-fold>
+    
+    
+    
+    
+}
