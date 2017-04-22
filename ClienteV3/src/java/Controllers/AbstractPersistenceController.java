4449f8a59dbc68dada875d35710cf72deb3511ed
Kmilo <Kmilo@PC-Kmilo>
2017-04-21 20:26:37 -0500

    Regenerando el proyecto, porque presenta el error de no def class found

--- ClienteV3/src/java/Controllers/AbstractPersistenceController.java
+++ ClienteV3/src/java/Controllers/AbstractPersistenceController.java
@@ -15,7 +15,6 @@ import java.io.Serializable;
 import java.util.Date;
 import java.util.Iterator;
 import java.util.List;
-import java.util.ResourceBundle;
 import java.util.Set;
 import java.util.logging.Level;
 import java.util.logging.Logger;
